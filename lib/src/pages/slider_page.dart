ea0e60fbb69bb5e70785126fef89f002fdd7dc03
Dante Alferez Condori <dalcon@hotmail.com>
2021-04-16 21:10:15 -0500

    Se agrega dos páginas de scroll y listas.

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
-
 class SliderPage extends StatefulWidget {
   @override
   _SliderPageState createState() => _SliderPageState();
 }
 
 class _SliderPageState extends State<SliderPage> {
-
   double _valorSlider = 100.0;
+  bool _bloquearCheck = false;
 
   @override
   Widget build(BuildContext context) {
@@ -17,39 +16,69 @@ class _SliderPageState extends State<SliderPage> {
         title: Text('Slider'),
       ),
       body: Container(
-        padding: EdgeInsets.only(top: 50.0),
-        child: Column(
-          children: <Widget>[
-            _crearSlider(),
-            Expanded(
-              child: _crearImagen()
-            ),
-          ],
-        )
-      ),
+          padding: EdgeInsets.only(top: 50.0),
+          child: Column(
+            children: <Widget>[
+              _crearSlider(),
+              _checkBox(),
+              _crearSwitch(),
+              Expanded(child: _crearImagen()),
+            ],
+          )),
     );
   }
 
   Widget _crearSlider() {
-
     return Slider(
-      activeColor: Colors.indigoAccent,
-      label: 'Tamaño de la imagen',
-      // divisions: 20,
-      value: _valorSlider,
-      min: 10.0,
-      max: 400.0,
-      onChanged: (valor) {
+        activeColor: Colors.indigoAccent,
+        label: 'Tamaño de la imagen',
+        // divisions: 20,
+        value: _valorSlider,
+        min: 10.0,
+        max: 400.0,
+        onChanged: _bloquearCheck
+            ? null
+            : (valor) {
+                setState(() {
+                  _valorSlider = valor;
+                });
+              });
+  }
+
+  Widget _checkBox() {
+    // return Checkbox(
+    //   value: _bloquearCheck,
+    //   onChanged: (valor) {
+    //     setState(() {
+    //       _bloquearCheck = valor;
+    //     });
+    //   });
+
+    return CheckboxListTile(
+        title: Text('Bloquear slider'),
+        value: _bloquearCheck,
+        onChanged: (valor) {
           setState(() {
-            _valorSlider = valor;
+            _bloquearCheck = valor;
           });
-      });
+        });
+  }
+
+  Widget _crearSwitch() {
+    return SwitchListTile(
+        title: Text('Bloquear slider'),
+        value: _bloquearCheck,
+        onChanged: (valor) {
+          setState(() {
+            _bloquearCheck = valor;
+          });
+        });
   }
 
   Widget _crearImagen() {
-
     return Image(
-      image: NetworkImage('https://assets1.ignimgs.com/2019/06/27/end-of-evangelion-br-1561679366459_160w.jpg'),
+      image: NetworkImage(
+          'https://assets1.ignimgs.com/2019/06/27/end-of-evangelion-br-1561679366459_160w.jpg'),
       width: _valorSlider,
       fit: BoxFit.contain,
     );
